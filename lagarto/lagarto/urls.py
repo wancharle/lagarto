38a17dd8dabe0fd16b76ef00654267ea1da20482
Wancharle S Quirino <wancharle@gmail.com>
2012-09-18 11:05:08 -0300

    menus funcionando.
    os menus agora mostram a sombra e exibem uma pagina padrão

--- lagarto/lagarto/urls.py
+++ lagarto/lagarto/urls.py
@@ -8,6 +8,10 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = patterns('',
     
     url(r'^$', 'django.views.generic.simple.direct_to_template',{'template':'lagarto/index.html'}, name='home'),
+    url(r'^pousada/$', 'django.views.generic.simple.direct_to_template',{'template':'lagarto/pousada.html'}, name='pousada'),
+    url(r'^estrutura/$', 'django.views.generic.simple.direct_to_template',{'template':'lagarto/estrutura.html'}, name='estrutura'),
+    url(r'^suites/$', 'django.views.generic.simple.direct_to_template',{'template':'lagarto/suites.html'}, name='suites'),
+    url(r'^localizacao/$', 'django.views.generic.simple.direct_to_template',{'template':'lagarto/localizacao.html'}, name='localizacao'),
 
     # url(r'^admin/', include(admin.site.urls)),
 )
