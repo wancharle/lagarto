d718188a2cde1ad3a99a206afbf1dce5e0cb6601
Wancharle S Quirino <wancharle@gmail.com>
2012-10-03 14:21:56 -0300

    suites com menu funcionando parcialmente e loader na capa

--- lagarto/static/js/lagarto.js
+++ lagarto/static/js/lagarto.js
@@ -4,20 +4,45 @@
 function menu_com_fade(container_id,menu_tag){
     
     $("#"+container_id + " " + menu_tag).click(function (){
-    $("#"+container_id + " " + menu_tag).removeClass("ativo");
 
-            var bloco = $(this).attr("data-bloco");
-            var menu = $(this);
-            menu.addClass("ativo");
-            $("#" + container_id +" div.bloco.ativo").hide();
-                $("#" + container_id + " div.bloco.ativo").removeClass('ativo');
-                $("#"+bloco).fadeIn();
-                $("#"+bloco).addClass("ativo");
+        $("#"+container_id + " " + menu_tag).removeClass("ativo");
+
+        var bloco = $(this).attr("data-bloco");
+        var menu = $(this);
+        menu.addClass("ativo");
+        $("#" + container_id +" div.bloco.ativo").hide();
+        $("#" + container_id + " div.bloco.ativo").removeClass('ativo');
+        $("#"+bloco).fadeIn();
+        $("#"+bloco).addClass("ativo");
            
-        });
+    });
 }
  
- 
+function menu_com_fade_suites(container_id,menu_tag){
+    
+    $("#"+container_id + " " + menu_tag).click(function (){
+
+        $("#"+container_id + " " + menu_tag).removeClass("ativo");
+        $("#"+container_id + " h2").removeClass("ativo");
+        $("#"+container_id + " li").removeClass("ativo");
+
+        if (menu_tag == "li"){
+            $(this).parent().prev("h2").addClass("ativo");    
+        }
+        if (menu_tag =="h2"){
+
+            $(this).next().children().first().addClass("ativo");    
+        }
+        var bloco = $(this).attr("data-bloco");
+        var menu = $(this);
+        menu.addClass("ativo");
+        $("#" + container_id +" div.bloco.ativo").hide();
+        $("#" + container_id + " div.bloco.ativo").removeClass('ativo');
+        $("#"+bloco).fadeIn();
+        $("#"+bloco).addClass("ativo");
+           
+    });
+}
 
 function parseURLParams(url) {
   var queryStart = url.indexOf("?") + 1;
