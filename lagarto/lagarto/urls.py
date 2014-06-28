c33ba6c22dfa4e6a67102b733d69eadd51c2ffaa
Wancharle <wancharle@gmail.com>
2014-06-28 17:24:51 -0300

    corrigindo urls para migração para novo django

--- lagarto/lagarto/urls.py
+++ lagarto/lagarto/urls.py
@@ -1,17 +1,17 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
+from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     
-    url(r'^$', 'django.views.generic.simple.direct_to_template',{'template':'lagarto/index.html'}, name='home'),
-    url(r'^pousada/$', 'django.views.generic.simple.direct_to_template',{'template':'lagarto/pousada.html'}, name='pousada'),
-    url(r'^estrutura/$', 'django.views.generic.simple.direct_to_template',{'template':'lagarto/estrutura.html'}, name='estrutura'),
-    url(r'^suites/$', 'django.views.generic.simple.direct_to_template',{'template':'lagarto/suites.html'}, name='suites'),
-    url(r'^localizacao/$', 'django.views.generic.simple.direct_to_template',{'template':'lagarto/localizacao.html'}, name='localizacao'),
+    url(r'^$', TemplateView.as_view(template_name='lagarto/index.html'), name='home'),
+    url(r'^pousada/$', TemplateView.as_view(template_name='lagarto/pousada.html'), name='pousada'),
+    url(r'^estrutura/$',TemplateView.as_view(template_name='lagarto/estrutura.html'), name='estrutura'),
+    url(r'^suites/$', TemplateView.as_view(template_name='lagarto/suites.html'), name='suites'),
+    url(r'^localizacao/$', TemplateView.as_view(template_name='lagarto/localizacao.html'), name='localizacao'),
 
    (r'^i18n/', include('django.conf.urls.i18n')),
     (r'^newsletter/cancela/(\d+)/(\d+)/$','newsletter.views.cancela_newsletter'),  
