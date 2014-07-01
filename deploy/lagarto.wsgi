65a4466f8ef78187baca8362255d549cd01679b5
Wancharle <wancharle@gmail.com>
2014-07-01 14:22:18 -0300

    corrigindo acentos

--- deploy/lagarto.wsgi
+++ deploy/lagarto.wsgi
@@ -3,6 +3,8 @@ sys.path.append('/home/rabodolagarto/apps_wsgi/lagarto')
 sys.path.append('/home/rabodolagarto/apps_wsgi/lagarto/lagarto')
 os.environ['PYTHON_EGG_CACHE'] = '/home/rabodolagarto/apps_wsgi/.python-eggs'
 os.environ['DJANGO_SETTINGS_MODULE']='lagarto.settings.local'
+os.environ['LC_ALL'] = 'pt_BR.UTF-8'
+os.environ['LANG'] = 'pt_BR.UTF-8'
 
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
--- lagarto/paginas/models.py
+++ lagarto/paginas/models.py
@@ -4,13 +4,13 @@ from django.db import models
 from image_cropping import ImageRatioField
 
 class Slider(models.Model):
-    foto = models.ImageField(upload_to="slide/", help_text=u"imagens com proporção 5x2. Exemplo: 1440px X 564px")
+    foto = models.ImageField(upload_to=u"slide/", help_text=u"imagens com proporção 5x2. Exemplo: 1440px X 564px")
     cropping = ImageRatioField('foto', '1440x564')
 
     legenda = models.CharField(max_length=100)
     legenda_em_ingles = models.CharField(max_length=100,verbose_name=u"legenda em inglês")
 
-    url = models.CharField(max_length=100, db_index=True,null=True,blank=True)
+    url = models.CharField(max_length=100, db_index=True,null=True,blank=False)
     ordem = models.IntegerField(default=0)
 
     class Meta:
